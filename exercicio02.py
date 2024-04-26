0b4b015aff6348727d87e90b91832fbc3c82b470
kaylane <kaka@localhost.localdomain>
2024-04-26 22:01:14 +1100

    Exercicio02 Concluido

---
+++ exercicio02.py
@@ -0,0 +1,10 @@
+def divisao():
+    num1=int(input("Informe um numero:"))
+    num2=int(input("Informe um numero:"))
+    if num2==0:
+        print("Infinito.")
+    div=num1/num2
+    print(div)
+    return
+
+divisao()

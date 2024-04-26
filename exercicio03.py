a813ef29e6fc7b85f94fca24a7fbef6b42d3c0a1
kaylane <kaka@localhost.localdomain>
2024-04-26 22:03:20 +1100

    Exercicio03 Concluido

---
+++ exercicio03.py
@@ -0,0 +1,15 @@
+def cpf():
+    digito=input("Qual o numero do seu CPF:")
+    if 11 == len(digito):
+        print("Numero de CPF valido!")
+    else:
+        print("Seu cpf nao é valido!")
+cpf()
+
+
+
+
+
+
+
+

f6e44ebc3f27abeb05924e0be1a354640342937d
kaylane <kaka@localhost.localdomain>
2024-04-26 21:48:06 +1100

    Exercicio01 Concluido

---
+++ exercicio01.py
@@ -0,0 +1,10 @@
+def verificar():
+    num=int(input("Informe um numero inteiro:"))
+    if num>0:
+        print("Numero positivo.")
+    elif num==0:
+        print("Numero neutro.")
+    else:
+        print("Numero negativo.")
+
+verificar()
